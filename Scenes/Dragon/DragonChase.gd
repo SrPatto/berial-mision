480fc56145f6b162d356c41d8557fd3d4a7a5474
SrPatto <ladrianrv@hotmail.com>
2025-04-18 01:23:29 -0600

    Dragon basic movement
    - Dragon Finite State Machine
    
    Dragon Chase State

---
+++ Scenes/Dragon/DragonChase.gd
@@ -0,0 +1,37 @@
+extends State_Dragon
+
+func Enter():
+	player = get_tree().get_first_node_in_group("Player")
+	dragon = $"../.."
+	navigation_agent_3d = $"../../NavigationAgent3D"
+	pass
+	
+func Exit():
+	pass
+	
+func Update(_delta: float):
+	Make_Path(player.global_position)
+	pass
+	
+func Physics_Update(_delta: float):
+	pass
+
+func Make_Path(target_position: Vector3):
+	navigation_agent_3d.set_target_position(target_position)
+	var current_agent_position = dragon.global_position
+	var next_path_position = navigation_agent_3d.get_next_path_position() 
+	var direction = (next_path_position - current_agent_position).normalized()
+	dragon.velocity = direction * move_speed
+	
+	# Smooth look_at
+	var current_rotation = dragon.rotation.y
+	var target_rotation = atan2(direction.x, direction.z)
+	dragon.rotation.y = lerp_angle(current_rotation, target_rotation, 0.1) # Adjust 0.1 for smoothness
+	
+	if navigation_agent_3d.avoidance_enabled:
+		dragon.set_velocity(direction * move_speed)
+	else:
+		_on_navigation_agent_3d_velocity_computed(direction)
+
+func _on_navigation_agent_3d_velocity_computed(safe_position: Vector3):
+	dragon.velocity = safe_position * move_speed
---
+++ Scripts/Dragon/StateDragon.gd
@@ -0,0 +1,9 @@
+extends State
+
+class_name State_Dragon
+
+var collision_base: CollisionShape3D 
+var navigation_agent_3d: NavigationAgent3D
+var player: CharacterBody3D
+var dragon: CharacterBody3D
+var move_speed := 4
